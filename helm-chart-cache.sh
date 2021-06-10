e1548827a97446423bcc26a5339438c7bae29ed7
Rune Juhl Jacobsen <runejuhl@enableit.dk>
2021-06-10 13:42:28 +0200

    Lint scripts, add missing scripts from https://github.com/blackwoodseven/k8s-init/

--- helm-chart-cache.sh
+++ helm-chart-cache.sh
@@ -10,38 +10,34 @@ do
         p) password=${OPTARG};;
         r) registry=${OPTARG};;
         u) username=${OPTARG};;
+        *) >&2 echo "Invalid flag '$flag', exiting"; exit 1
     esac
 done
 
-helm registry login $registry --username $username --password $password
+helm registry login "$registry" --username "$username" --password-stdin <<< "$password"
 
-set +e
-for path in $(find argocd-helm-charts -maxdepth 1 -mindepth 1 -type d);
-do
-	echo "### Attempting dep up for upstream for $path ###"
-	bash helm-dep-up.sh -u true -p $path -r $registry;
-	if [ -d "$path/charts" ]; then
-		for tars in $path/charts/*.tgz
-		do
-			tgzfile=$(basename $tars);
-			version=$(echo $tgzfile | grep -o "v*[0-9]\{1\}[0-9]*.+*[0-9].*" | sed 's/\.tgz//')
-			chartname=$(echo $tgzfile | sed "s/-$version.tgz//")
-			# The logic behind this if statement is sometimes version is just a single digit and while pulling
-			# the chart for such they fail hence append 0.0
-			if [ -z "$version" ]; then
-				version=$(echo $tgzfile | grep -o "v*[0-9]\{1\}.tgz" | sed 's/\.tgz//')
-				chartname=$(echo $tgzfile | sed "s/-$version.tgz//")
-				version=$version.0.0
-			fi
-			echo "### Pulling chart $chartname:$version ###"
-			helm chart pull $registry/$chartname:$version > /dev/null
-			if [ $? -ne 0 ]; then
-			    echo "### Saving and pushing chart $chartname:$version ###"
-			    helm chart save $tars $registry/$chartname:$version;
-			    helm chart push $registry/$chartname:$version;
-			    echo "###  Doing dep up for ghcr for $path ###"
-			    bash helm-dep-up.sh -u false -p $path -r $registry;
-			fi
-		done
-    fi
+find argocd-helm-charts -maxdepth 1 -mindepth 1 -type d | while read -r path; do
+  echo "### Attempting dep up for upstream for $path ###"
+  bash helm-dep-up.sh -u true -p "$path" -r "$registry"
+  if [ -d "${path}/charts" ]; then
+    find "${path}/charts" -maxdepth 1 -name '*.tgz' | while read -r tarfile; do
+      tgzfile=$(basename "$tarfile")
+      version=$(echo "$tgzfile" | grep -o "v*[0-9]\{1\}[0-9]*.+*[0-9].*" | sed 's/\.tgz//')
+      # The logic behind this if statement is sometimes version is just a single
+      # digit and while pulling the chart for such they fail hence append 0.0
+      if [ -z "$version" ]; then
+        version=$(echo "$tgzfile" | grep -o "v*[0-9]\{1\}.tgz" | sed 's/\.tgz//')
+        version=$version.0.0
+      fi
+      chartname=${tgzfile/-$version.tgz}
+      echo "### Pulling chart $chartname:$version ###"
+      if ! helm chart pull "${registry}/${chartname}:${version}" > /dev/null; then
+        echo "### Saving and pushing chart $chartname:$version ###"
+        helm chart save "$tarfile" "${registry}/${chartname}:${version}"
+        helm chart push "${registry}/${chartname}:${version}"
+        echo "###  Doing dep up for ghcr for $path ###"
+        bash helm-dep-up.sh -u false -p "$path" -r "$registry"
+      fi
+    done
+  fi
 done
