f2b81c73bc30aab4a16ab3ffd42931ed633276aa
Ashish Jaiswal <ashish@obmondo.com>
2021-12-23 13:56:33 +0530

    Written puppet hiera generator script, which support encrypting the private keys

--- cluster-setup-scripts/puppet/generate-puppet-hiera.sh
+++ cluster-setup-scripts/puppet/generate-puppet-hiera.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Documentation
-# It will drop the yaml files into cluster-setup-scripts/puppet/<cluster-name>
+# It will drop the yaml files into kubernetes-config-<customer-id>/<cluster-name>
 # You will have to simply drop the yaml file and add any more extra keys
+#
+# This scripts expect few things and does not do sanity check, so watch out for that
+# It expects that both of these git repo sits in a same dir
+# ssh://git@gitlab.enableit.dk:2223/kubernetes/kubernetes-config-<customer-id>.git
+# ssh://git@gitlab.enableit.dk:2223/kubernetes/argocd-apps.git
 #
 # Example
 # ./cluster-setup-scripts/puppet/generate-puppet-hiera.sh \
@@ -31,6 +36,7 @@ Usage $0 [OPTION]:
   --kubetool-version              kubetool version                   Default=6.2.0
   --eyaml-public-key              eyaml public key path, expects it
                                   to be under cluster-setup-scripts/puppet Default=public_key.pkcs7.pem
+  --customer-id                   customer_id of a customer          Default=enableit
   -h | --help
 
 Example:
@@ -45,8 +51,10 @@ fi
 
 declare CONTAINER_RT=docker
 declare KUBETOOL_VERSION=6.2.0
-declare -a ETCD_CLUSTERS_IPS=()
 declare EYAML_PUBLIC_KEY=public_key.pkcs7.pem
+declare CUSTOMER_ID=enableit
+declare -a ETCD_CLUSTERS_IPS=()
+declare -a ETCD_CLUSTERS_EXTRA_SANS=()
 
 while [[ $# -gt 0 ]]; do
   arg="$1"
@@ -67,9 +75,7 @@ while [[ $# -gt 0 ]]; do
     # docker and cri_containerd is supported.
     CONTAINER_RT=$1
 
-    echo -n "You have given ${CONTAINER_RT}, but this only works with or
-          you can tested with docker only.
-          so defaulting to docker"
+    echo -n "You have given ${CONTAINER_RT}, but this only works with docker, so defaulting to docker"
     shift
     ;;
   --san-domains)
@@ -87,6 +93,11 @@ while [[ $# -gt 0 ]]; do
 
     shift
     ;;
+  --customer-id)
+    CUSTOMER_ID=$1
+
+    shift
+    ;;
   -h|--help)
     ARGFAIL
 
@@ -99,6 +110,11 @@ done
 if [ -z "$CLUSTER_NAME" ]; then
   echo "no cluster name is given, please give one"
   exit 1
+else
+  if ! host "$CLUSTER_NAME" >/dev/null; then
+    echo "$CLUSTER_NAME is not resolving, we can not proceed. Please add it in your DNS"
+    exit 1
+  fi
 fi
 
 if [ -z "$VERSION" ]; then
@@ -121,6 +137,7 @@ else
       ETCD_CLUSTERS_IPS+=("$domain")
     else
       ETCD_INITIAL_CLUSTER="$ETCD_INITIAL_CLUSTER$delim$domain:$domain"
+      ETCD_CLUSTERS_EXTRA_SANS+=("$domain")
     fi
     delim=","
   done
@@ -128,34 +145,14 @@ else
   unset IFS
 fi
 
-# lets not accept etcd cluster nodes ip, its better to find out from the san-domains list
-# if [ "${#ETCD_CLUSTERS_IPS[@]}" -eq 0 ]; then
-#   if [ -z "$SAN_DOMAINS" ]; then
-#     echo -e "Error: can not proceed without --etcd-cluters-ips or --san-domains
-# Need atleast one of the options"
-#     exit 1
-#   fi
-# else
-#   echo -e "************* NOTE *****************
-# You have not given --etcd-cluters-ips argument to the script. This script will try to identify the etcd cluster ips from the given --san-domains arguments
-# These are the below etcd clusters ip and their FQDN
-# If you believe this is wrong, better give the --etcd-cluters-ips options"
-#   echo "${ETCD_CLUSTERS_IPS[@]}"
-#
-#   read -r -p "Type true or false: " CONFIRMATION
-#
-#   case $CONFIRMATION in
-#     true)
-#       echo "Going ahead with the identified etcd clusters ips"
-#       ;;
-#     false|*)
-#       echo "Great, you don't trust the given etcd clusters ips, exiting"
-#       exit 1
-#       ;;
-#   esac
-# fi
+KUBERNETES_CONFIG_DIR="../kubernetes-config-${CUSTOMER_ID}/${CLUSTER_NAME}/hiera"
+BASEDIR=$(dirname "$0")
+_BASEDIR=${BASEDIR##./}
+_EYAML_PUBLIC_KEY="../../../argocd-apps/${_BASEDIR}/${EYAML_PUBLIC_KEY}"
 
-cd "$(pwd)/cluster-setup-scripts/puppet"
+# Create the required directory
+mkdir -p "${KUBERNETES_CONFIG_DIR}"
+cd "${KUBERNETES_CONFIG_DIR}"
 
 echo -e "OS=debian
 INSTALL_DASHBOARD=true
@@ -164,23 +161,16 @@ ETCD_INITIAL_CLUSTER=${ETCD_INITIAL_CLUSTER}
 ETCD_IP=\"%{networking.ip}\"
 KUBE_API_ADVERTISE_ADDRESS=\"%{networking.ip}\"
 VERSION=${VERSION}
-CONTAINER_RUNTIME=${CONTAINER_RT}" > "${CLUSTER_NAME}"_kubetool_setup_env
-
-
-#echo "$ETCD_CLUSTERS_IPS"
-# Create the required directory
-mkdir -p "$(pwd)/${CLUSTER_NAME}"
+CONTAINER_RUNTIME=${CONTAINER_RT}" > kubetool_setup_env
 
 # Generate the k8s hiera yaml files
-docker run --rm -v "$(pwd)/${CLUSTER_NAME}":/mnt --env-file "${CLUSTER_NAME}"_kubetool_setup_env puppet/kubetool:"${KUBETOOL_VERSION}"
+docker run --rm -v "$(pwd)":/mnt --env-file kubetool_setup_env puppet/kubetool:"${KUBETOOL_VERSION}"
 
-
-# NOTE
-# docker generate the yaml file with root perms
-sudo chown -R "$USER:$USER" "$(pwd)/${CLUSTER_NAME}"
+# NOTE: docker generate the yaml file with root perms
+sudo chown -R "$USER:$USER" ./*
 
 # Encrypt the keys
-#cat htzsb45fsn1a.enableit.dk.yaml | yq eval '."kubernetes::etcdclient_key"' - | eyaml encrypt --stdin --output=block --pkcs7-public-key=../public_key.pkcs7.pem -
+# cat htzsb45fsn1a.enableit.dk.yaml | yq eval '."kubernetes::etcdclient_key"' - | eyaml encrypt --stdin --output=block --pkcs7-public-key=../public_key.pkcs7.pem -
 for ETCD_NODE in "${ETCD_CLUSTERS_IPS[@]}"; do
   NODE_FILENAME=$(echo "${ETCD_NODE}" | cut -d ':' -f1)
 
@@ -190,14 +180,11 @@ for ETCD_NODE in "${ETCD_CLUSTERS_IPS[@]}"; do
   # kubernetes::etcdclient_key
 
   for NODE_KEYS in etcdserver_key etcdpeer_key etcdclient_key; do
-    _NODE_KEYS=$(yq eval ".\"kubernetes::${NODE_KEYS}\"" "$(pwd)/${CLUSTER_NAME}/${NODE_FILENAME}".yaml \
-                  | eyaml encrypt \
-                  --stdin --output=string \
-                  --pkcs7-public-key="${EYAML_PUBLIC_KEY}" -)
+    _NODE_KEYS=$(yq eval ".\"kubernetes::${NODE_KEYS}\"" "${NODE_FILENAME}".yaml \
+                  | eyaml encrypt --stdin --output=string --pkcs7-public-key="${_EYAML_PUBLIC_KEY}" -)
 
-    yq eval --inplace ".\"kubernetes::${NODE_KEYS}\" = \"${_NODE_KEYS}\"" "$(pwd)/${CLUSTER_NAME}/${NODE_FILENAME}".yaml
+    yq eval --inplace ".\"kubernetes::${NODE_KEYS}\" = \"${_NODE_KEYS}\"" "${NODE_FILENAME}".yaml
   done
-
 done
 
 # NOTE
@@ -209,27 +196,38 @@ done
 #            Not After : Dec 21 21:50:00 2026 GMT
 #        Subject: CN = client
 
-yq eval --inplace "del(.\"kubernetes::etcdclient_key\")" "$(pwd)/${CLUSTER_NAME}"/Debian.yaml
-yq eval --inplace "del(.\"kubernetes::etcdclient_crt\")" "$(pwd)/${CLUSTER_NAME}"/Debian.yaml
+yq eval --inplace "del(.\"kubernetes::etcdclient_key\")" Debian.yaml
+yq eval --inplace "del(.\"kubernetes::etcdclient_crt\")" Debian.yaml
 
 # Let's encrypt the Debian.yaml private keys
 # Need to encrypt these keys from the OS family file
 # kubernetes::etcd_ca_key
 # kubernetes::kubernetes_ca_key
 # kubernetes::discovery_token_hash
-# kubernetes::kubernetes_front_proxy_ca_key
 # kubernetes::sa_key
-for OSFAMILY_KEYS in etcd_ca_key kubernetes_ca_key discovery_token_hash kubernetes_front_proxy_ca_key sa_key; do
-  _OSFAMILY_KEYS=$(yq eval ".\"kubernetes::${OSFAMILY_KEYS}\"" "$(pwd)/${CLUSTER_NAME}"/Debian.yaml \
+# kubernetes::token
+for OSFAMILY_KEYS in etcd_ca_key kubernetes_ca_key discovery_token_hash sa_key token; do
+  _OSFAMILY_KEYS=$(yq eval ".\"kubernetes::${OSFAMILY_KEYS}\"" Debian.yaml \
                   | eyaml encrypt \
                   --stdin --output=string \
-                  --pkcs7-public-key="${EYAML_PUBLIC_KEY}" -)
+                  --pkcs7-public-key="${_EYAML_PUBLIC_KEY}" -)
 
-  yq eval --inplace ".\"kubernetes::${OSFAMILY_KEYS}\" = \"${_OSFAMILY_KEYS}\"" "$(pwd)/${CLUSTER_NAME}"/Debian.yaml
+  yq eval --inplace ".\"kubernetes::${OSFAMILY_KEYS}\" = \"${_OSFAMILY_KEYS}\"" Debian.yaml
 done
 
-# Delete the peer list, so we can only required one which are actual etcd nodes.
-yq eval --inplace "del(.\"kubernetes::etcd_peers\")" "$(pwd)/${CLUSTER_NAME}"/Debian.yaml
+# Change the key name as per puppet role
+# TODO: oneliner would be nice, but a quick solution is not bad though.
+FRONT_CA_KEY=$(yq eval ".\"kubernetes::kubernetes_front_proxy_ca_key\"" Debian.yaml)
+yq eval --inplace ".\"kubernetes::front_proxy_ca_key\" = \"${FRONT_CA_KEY}\"" Debian.yaml
+FRONT_CA_CERT=$(yq eval ".\"kubernetes::kubernetes_front_proxy_ca_crt\"" Debian.yaml)
+yq eval --inplace ".\"kubernetes::front_proxy_ca_crt\" = \"${FRONT_CA_CERT}\"" Debian.yaml
+
+# Delete these keys, since will setup our own keys based on the role::virtualization::kubernetes class
+yq eval --inplace "del(.\"kubernetes::etcd_peers\")" Debian.yaml
+yq eval --inplace "del(.\"kubernetes::kubernetes_front_proxy_ca_crt\")" Debian.yaml
+yq eval --inplace "del(.\"kubernetes::kubernetes_front_proxy_ca_key\")" Debian.yaml
+yq eval --inplace "del(.\"kubernetes::kubernetes_package_version\")" Debian.yaml
+yq eval --inplace "del(.\"kubernetes::kubernetes_version\")" Debian.yaml
 
 # Let's encrypt the few bad keys which comes from the kubetool command.
 ETCD_CLUSTER_NODES=""
@@ -241,22 +239,51 @@ for IPS in "${ETCD_CLUSTERS_IPS[@]}"; do
   delim=","
 
   # Fix the peers list
-  yq eval --inplace ".\"kubernetes::etcd_peers\" += [\"${NODE_IP}\"]" "$(pwd)/${CLUSTER_NAME}"/Debian.yaml
+  yq eval --inplace ".\"kubernetes::etcd_peers\" += [\"${NODE_IP}\"]" Debian.yaml
 done
 
 # Fix the initial_cluster setting
-yq eval --inplace ".\"kubernetes::etcd_initial_cluster\" = \"${ETCD_CLUSTER_NODES}\"" "$(pwd)/${CLUSTER_NAME}"/Debian.yaml
+yq eval --inplace ".\"kubernetes::etcd_initial_cluster\" = \"${ETCD_CLUSTER_NODES}\"" Debian.yaml
 
 # Let's fix the calico version as well
-
-# NOTE
-# We are using 3.18 for now, since this is what we having been using and dont want any surprises.
-yq eval --inplace ".\"kubernetes::cni_network_provider\" = \"https://docs.projectcalico.org/v3.18/manifests/calico.yaml\"" "$(pwd)/${CLUSTER_NAME}"/Debian.yaml
+# NOTE: We are using 3.18 for now, since this is what we having been using and dont want any surprises.
+yq eval --inplace ".\"kubernetes::cni_network_provider\" = \"https://docs.projectcalico.org/v3.18/manifests/calico.yaml\"" Debian.yaml
 
 # Fix the controller address
-# NOTE
-# We expect the cluster-name is a domain which can resolve.
-yq eval --inplace ".\"kubernetes::controller_address\" = \"${CLUSTER_NAME}:6443\"" "$(pwd)/${CLUSTER_NAME}"/Debian.yaml
+# NOTE: We expect the cluster-name is a domain which can resolve.
+yq eval --inplace ".\"kubernetes::controller_address\" = \"${CLUSTER_NAME}:6443\"" Debian.yaml
+
+# Lets add the extra SANS cert
+for DOMAIN in "${ETCD_CLUSTERS_EXTRA_SANS[@]}"; do
+  yq eval --inplace ".\"kubernetes::apiserver_cert_extra_sans\" += [\"${DOMAIN}\"]" Debian.yaml
+
+  # Remove these files as well
+  rm -f "${DOMAIN}".yaml
+done
+
+# Lets add the api_server_count
+yq eval --inplace ".\"kubernetes::api_server_count\" = ${#ETCD_CLUSTERS_IPS[@]}" Debian.yaml
 
 # lets change all the keys
-grep -l '^kubernetes::' "$(pwd)/${CLUSTER_NAME}"/* | xargs sed -i 's/kubernetes/role::virtualization::kubernetes/g'
+grep -l '^kubernetes::' ./* | xargs sed -i 's/^kubernetes/role::virtualization::kubernetes/g'
+
+# Lets have some sane defaults
+yq eval --inplace ".classes += [\"role::virtualization::kubernetes\"]" Debian.yaml
+yq eval --inplace ".\"monitor::enable\" = false" Debian.yaml
+yq eval --inplace ".\"common::network::firewall::enable\" = true" Debian.yaml
+yq eval --inplace ".\"common::network::firewall::allow_docker\" = true" Debian.yaml
+yq eval --inplace ".\"common::network::firewall::allow_k8s\" = true" Debian.yaml
+yq eval --inplace ".\"common::network::firewall::enable_forwarding\" = true" Debian.yaml
+yq eval --inplace ".\"common::monitor::exporter::ssl::enable\" = false" Debian.yaml
+yq eval --inplace ".\"role::virtualization::kubernetes::extra_public_ports\" = [3025]" Debian.yaml
+yq eval --inplace ".\"role::virtualization::kubernetes::role\" = \"controller\"" Debian.yaml
+yq eval --inplace ".\"role::virtualization::kubernetes::version\" = \"${VERSION}\"" Debian.yaml
+
+#- r2d2.enableit.dk # klavs home
+TRUSTED_SOURCES=(109.238.49.196/28 bantha.enableit.dk atat.enableit.dk r2d2.enableit.dk)
+for SOURCE in "${TRUSTED_SOURCES[@]}"; do
+  yq eval --inplace ".\"role::virtualization::kubernetes::allow_k8s_api\" += [\"${SOURCE}\"]" Debian.yaml
+done
+
+# lets just sort the keys
+yq eval --inplace 'sort_keys(.)' Debian.yaml
