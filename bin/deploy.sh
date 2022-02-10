cf3361a401f1c36d6f49ecaa0afa1be1a4b9edbd
Rune Juhl Jacobsen <runejuhl@enableit.dk>
2022-02-10 22:55:55 +0100

    Revert some change trying to fix CI

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,6 +8,11 @@ if ! [[ "${KUBERNETES_CONFIG_REPO_URL}" && "${KUBERNETES_CONFIG_REPO_TOKEN}" ]];
   exit 42
 fi
 
+if ! [[ "${CI}" ]]; then
+  >&2 echo "This script should only be run in a CI pipeline. This is not a CI pipeline."
+  exit 41
+fi
+
 case "${KUBERNETES_CONFIG_REPO_URL}" in
   *gitlab*)
     config_repo_username=oauth2
@@ -31,34 +36,21 @@ if ! [[ "${KUBERNETES_CONFIG_REPO_URL}" =~ ^https://(.+) ]]; then
 fi
 
 config_repo_url_with_auth="https://${config_repo_auth}@${KUBERNETES_CONFIG_REPO_URL:8}"
-deploy_target_branch="${OBMONDO_DEPLOY_TARGET_BRANCH:-master}"
+deploy_target_branch="${OBMONDO_DEPLOY_TARGET_BRANCH:-main}"
 config_repo_name=$(echo "$KUBERNETES_CONFIG_REPO_URL" | sed -r "s/.+\/(.+)\..+/\1/")
 config_repo_path="../${config_repo_name}"
 
-if [[ "$GITLAB_USER_EMAIL" ]];
-then
-    git config --global user.email "${GITLAB_USER_EMAIL}"
-fi
+git config --global user.email "${GITLAB_USER_EMAIL}"
+git config --global user.name "${GITLAB_USER_NAME}"
 
-if [[ "$GITLAB_USER_NAME" ]];
-then
-    git config --global user.name "${GITLAB_USER_NAME}"
-fi
-
-if [[ ! -d $config_repo_path ]];
-then
-    git clone "${config_repo_url_with_auth}" "${config_repo_path}"
-    git -C "${config_repo_path}" checkout -b "deploy-${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}" --track "origin/${deploy_target_branch}"
-fi
+git clone "${config_repo_url_with_auth}" "${config_repo_path}"
+git -C "${config_repo_path}" checkout -b "deploy-${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}" --track "origin/${deploy_target_branch}"
 
 # Loop over all clusters that are defined in config repo and copy
 # corresponding compiled files into cloned repo.
-find "${config_repo_path}" -mindepth 1 -maxdepth 1 -type d -not -name .\* | while read -r cluster_dir; do
-    cluster="$(basename "${cluster_dir}")"
-    # cluster_jsonnet="${cluster_dir}/${cluster}-vars.jsonnet"
+find "${config_repo_path}/k8s/" -mindepth 1 -maxdepth 1 -type d -not -name .\* | while read -r cluster_dir; do
     ./build/kube-prometheus/build.sh "$cluster_dir"
-
-    git -C "${config_repo_path}" add -f "${cluster}/kube-prometheus"
+    git -C "${config_repo_path}" add -f "${cluster_dir}"
 done
 
 # Check if we have modifications to commit
