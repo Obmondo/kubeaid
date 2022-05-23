c753acb9c13c1b1118f65a8057f44dbd941958f6
Aman Shah <aman@enableit.dk>
2022-05-23 13:12:22 +0530

    Add a setting to enable auto scaling on kube cluster

--- cluster-setup-files/terraform/aks/aks.tf
+++ cluster-setup-files/terraform/aks/aks.tf
@@ -10,9 +10,12 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     kubernetes_version  = var.kubernetes_version
 
     default_node_pool {
-        name            = "agentpool"
-        node_count      = var.agent_count
-        vm_size         = var.vm_size
+        name                = "agentpool"
+        node_count          = var.agent_count
+        vm_size             = var.vm_size
+        enable_auto_scaling = var.enable_auto_scaling
+        min_count           = var.min_node_count 
+        max_count           = var.max_node_count
     }
 
     identity {
