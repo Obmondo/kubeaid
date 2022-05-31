4599af5b766c1c80d5346c3e6db7ff02d3f4ee0d
Aman Shah <aman@enableit.dk>
2022-05-31 18:07:19 +0530

    Added the support to add cloudfare issuer type

--- cluster-setup-files/terraform/aks/providers.tf
+++ cluster-setup-files/terraform/aks/providers.tf
@@ -8,42 +8,10 @@ terraform {
     kubernetes = {
       version = ">= 1.22.6"
     }
-
-   cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = ">=3.9.1"
-    }
-
-    helm = {
-      source  = "helm"
-      version = ">=2.4.1"
-    }
   }
 }
 
 # subscription_id will change based on which subscription VM will be added to
 provider "azurerm" {
     features {}
-}
-
-provider "cloudflare" {
-   api_token = var.letsencrypt_cloudflare_api_token
-}
-
-provider "helm" {
-  kubernetes {
-    host = azurerm_kubernetes_cluster.k8s.kube_config.0.host
-
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
-  }
-}
-
-provider "kubernetes" {
-  host = azurerm_kubernetes_cluster.k8s.kube_config.0.host
-
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
 }
