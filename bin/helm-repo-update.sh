8beda5133539adec1838fb33d497598fbb9652d2
Ashish Jaiswal <ashish@enableit.dk>
2022-07-07 05:50:05 +0530

    Patched the helm update script and fixed few chart.yaml to work with it.

--- bin/helm-repo-update.sh
+++ bin/helm-repo-update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -x
 set -eou pipefail
 
 HELM_APP="${1:-argocd-helm-charts}"
+MERGE_REQUEST="${2:-false}"
 
 for program in helm tar; do
   if ! command -v "$program" >/dev/null; then
@@ -45,34 +47,45 @@ function update_helm_chart {
   if [ "$HELM_CHART_DEP_PRESENT" -ne 0 ]; then
 
     # Add the repo
-    if ! helm repo list -o yaml | yq eval -e ".[].name == \"$HELM_REPO_NAME\"" >/dev/null; then
+    if ! helm repo list -o yaml | yq eval -e ".[].name == \"$HELM_REPO_NAME\"" >/dev/null 2>/dev/null; then
       helm repo add "$HELM_REPO_NAME" "$HELM_REPOSITORY_URL" >/dev/null
     fi
 
-    # helm search repo whoami/whoami --output yaml | yq eval '.[].version' -
-
     # Check if we have an upstream chart already present or not
     if test -f "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME/Chart.yaml"; then
-      HELM_UPSTREAM_APP_VERSION=$(yq eval '.version' "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME/Chart.yaml")
+      HELM_UPSTREAM_CHART_VERSION=$(helm search repo --regexp "${HELM_REPO_NAME}/${HELM_CHART_NAME}[^-]" --version ">=$HELM_CHART_VERSION" --output yaml | yq eval '.[].version' -)
 
       # Compare the version of upstream chart and our local chart
       # if there is difference, run helm dep up or else skip
-      if [ "$HELM_UPSTREAM_APP_VERSION" != "$HELM_CHART_VERSION" ]; then
+      if [ "$HELM_UPSTREAM_CHART_VERSION" != "$HELM_CHART_VERSION" ]; then
         echo "HELMING $HELM_CHART_NAME"
+
+        # Update the chart.yaml file
+        yq eval -i ".dependencies[].version = \"$HELM_UPSTREAM_CHART_VERSION\"" "$HELM_CHART_YAML"
+
         # Go to helm chart, 1st layer
         helm dependencies update "$HELM_CHART_PATH"
 
         # Untar the tgz file
-        tar -C "$HELM_CHART_DEP_PATH" -xvf "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME-$HELM_CHART_VERSION.tgz"
+        tar -C "$HELM_CHART_DEP_PATH" -xvf "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME-$HELM_UPSTREAM_CHART_VERSION.tgz"
 
-        # Remove the tar file, we dont need that.
-        rm -fr "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME-$HELM_CHART_VERSION.tgz"
+        # TODO: script does not support upstream chart dependency with file path
+        # need to sort out this later, for now only rook-ceph is the only chart which has this issue.
+        if ! grep -ir 'file://' "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME/Chart.yaml"; then
 
-        # Go to helm chart, 2nd layer
-        helm dependencies update "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME"
+          # some chart has some dependencies, but those dependencies are removed from upstream repo
+          # f.ex yetibot depends on postgresql on version 8.6.12 (but 8.6.12 is removed from bitnami helm chart repo)
+
+          if [ "$HELM_REPO_NAME" == "yetibot" ] || [ "$HELM_REPO_NAME" == "keycloak" ]; then
+            echo "Skipping $HELM_REPO_NAME helm dependency update, due to dependency being obsolete"
+          else
+            # Go to helm chart, 2nd layer
+            helm dependencies update "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME"
+          fi
+
+        fi
       else
-        echo "Helm chart $HELM_REPO_NAME matches with the version $HELM_CHART_VERSION given in Chart.yaml"
-
+        echo "Helm chart $HELM_REPO_NAME matches with the version $HELM_CHART_VERSION given in $HELM_CHART_NAME/Chart.yaml"
       fi
     else
       echo "HELMING $HELM_CHART_NAME"
@@ -82,14 +95,67 @@ function update_helm_chart {
       # Untar the tgz file
       tar -C "$HELM_CHART_DEP_PATH" -xvf "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME-$HELM_CHART_VERSION.tgz"
 
-      # Remove the tar file, we dont need that.
-      rm -fr "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME-$HELM_CHART_VERSION.tgz"
-
       # Go to helm chart, 2nd layer
       helm dependencies update "$HELM_CHART_DEP_PATH/$HELM_CHART_NAME"
     fi
   fi
 }
+
+function merge_request() {
+  chart_name=$1
+
+  # Don't run for merge requests that target a branch that is not master/main.
+  #if [[ "${CI_COMMIT_REF_NAME}" != master ]] && [[ "${CI_COMMIT_REF_NAME}" != main ]]; then
+   # exit
+  #fi
+
+  config_repo_path=$(pwd)
+  deploy_target_branch="master"
+
+  git config --global user.email "${GITLAB_USER_EMAIL}"
+  git config --global user.name "${GITLAB_USER_NAME}"
+
+  # Set the remote url
+  git remote set-url origin "https://oauth2:${HELM_UPDATE_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}"
+
+  # Check if we have modifications to commit
+  CHANGES=$(git -C "${config_repo_path}" status --porcelain | wc -l)
+
+  if (( CHANGES > 0)); then
+    title="[CI] Helm Chart Update ${chart_name}"
+
+    git switch --create "${chart_name}" --track "origin/${deploy_target_branch}"
+
+    git -C "${config_repo_path}" status
+    git -C "${config_repo_path}" commit -m "${title}"
+
+    # shellcheck disable=SC2094
+    output=$(2>&1 git -C "${config_repo_path}" push \
+                  --force-with-lease \
+                  -o merge_request.create \
+                  -o merge_request.target="${deploy_target_branch}" \
+                  -o merge_request.title="${title}" \
+                  -o merge_request.remove_source_branch \
+                  -o merge_request.description="Auto-generated pull request from Obmondo K8id CI, created from changes by ${GITLAB_USER_NAME} (${GITLAB_USER_EMAIL})." \
+                  origin HEAD)
+    echo "${output}"
+
+    if grep -q WARNINGS <<< "${output}"; then
+      exit 1
+    fi
+  fi
+}
+
+if "${CI}" ; then
+  TEMPDIR=$(mktemp -d)
+
+  # NOTE: git diff does not work, cause gitlab only checkout only one branch
+  # so there is nothing to compare.
+  git clone "https://gitlab-ci-token:${CI_JOB_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}" "${TEMPDIR}"
+
+  cd "${TEMPDIR}"
+fi
+
 
 if [ "$HELM_APP" != "argocd-helm-charts" ]; then
   if test -d "$HELM_APP"; then
@@ -100,7 +166,16 @@ if [ "$HELM_APP" != "argocd-helm-charts" ]; then
     exit 1
   fi
 else
-  find "$HELM_APP" -maxdepth 1 -mindepth 1 -type d | while read -r path; do
+  find "$HELM_APP" -maxdepth 1 -mindepth 1 -type d | sort | while read -r path; do
     update_helm_chart "$path"
+
+    chart_name=$(echo "$path" | cut -d '/' -f2)
+
+    # Raise MR for each individual helm chart
+    if $MERGE_REQUEST; then
+      merge_request "$chart_name"
+    fi
   done
 fi
+
+find . -name '*tgz*' -delete
