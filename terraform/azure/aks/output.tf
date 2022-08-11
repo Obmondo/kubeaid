e2493800b13008a5e6429473de2504f19f7e3ac9
Ashish Jaiswal <ashish@obmondo.com>
2022-08-11 14:47:56 +0530

    bug: fixed when terraform module tries to setup argocd repo on a argocd server, and it fails cause it cant connect to it, so plain text helps to setup the connectivity via http, which is fine since its local and we are doing a port forwarding

--- terraform/azure/aks/output.tf
+++ terraform/azure/aks/output.tf
@@ -1,36 +1,40 @@
 output "kube_config" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config_raw
-    sensitive = true
+  value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  sensitive = true
 }
 
 output "client_key" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
-    sensitive = true
+  value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
+  sensitive = true
 }
 
 output "client_certificate" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
-    sensitive = true
+  value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
-    sensitive = true
+  value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
+  sensitive = true
 }
 
-output "host" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
-    sensitive = true
+output "k8s_fqdn" {
+  value = azurerm_kubernetes_cluster.k8s.fqdn
+}
+
+output "private_fqdn" {
+  value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+  sensitive = true
 }
 
 output "remote_virtual_network_id" {
-    value = jsondecode(base64decode(data.external.cluster_vnet_id.result["base64_encoded"]))
+  value = jsondecode(base64decode(data.external.cluster_vnet_id.result["base64_encoded"]))
 }
 
 output "virtual_network_name" {
-    value = jsondecode(base64decode(data.external.cluster_vnet_name.result["base64_encoded"]))
+  value = jsondecode(base64decode(data.external.cluster_vnet_name.result["base64_encoded"]))
 }
 
 output "private_dns_zone_name" {
-    value = join(".", slice(split(".", azurerm_kubernetes_cluster.k8s.private_fqdn), 1, length(split(".", azurerm_kubernetes_cluster.k8s.private_fqdn))))
+  value = join(".", slice(split(".", azurerm_kubernetes_cluster.k8s.private_fqdn), 1, length(split(".", azurerm_kubernetes_cluster.k8s.private_fqdn))))
 }
