24184b760f694d202bb118f1b229c855a386dd41
Ashish Jaiswal <ashish@enableit.dk>
2022-09-22 13:00:21 +0530

    [#7984] Misc fixes.
    1. Fixed the ssh login on ec2 instance
    2. Handle if cluster name is different then the given domain name.
    3. Added a resource limit on etcd cluster (cpu and memory)
    4. Removed argocd admin secret and argocd repo creation
       Since it was not idempotent and bit difficult to recovery as well.

--- terraform/helm/providers.tf
+++ terraform/helm/providers.tf
@@ -1,27 +1,3 @@
-terraform {
-  required_providers {
-    sealedsecret = {
-      source = "Mobaibio/sealedsecret"
-      version = "1.0.1"
-    }
-
-    argocd = {
-      source = "oboukili/argocd"
-      version = "3.2.0"
-    }
-
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.12.1"
-    }
-  }
-}
-
 provider "helm" {
   kubernetes {
     host                   = var.k8s_host
@@ -29,55 +5,4 @@ provider "helm" {
     client_key             = base64decode(var.k8s_client_key)
     cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
   }
-}
-
-provider "sealedsecret" {
-  controller_name      = "sealed-secrets"
-  controller_namespace = "system"
-
-  kubernetes {
-    host                   = var.k8s_host
-    client_certificate     = base64decode(var.k8s_client_certificate)
-    client_key             = base64decode(var.k8s_client_key)
-    cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
-  }
-}
-
-provider "random" {}
-
-provider "time" {}
-
-provider "argocd" {
-  server_addr = "localhost:8080"
-  username    = "admin"
-  password    = var.argocd_admin_password
-
-  # NOTE: this is a wierd bug, have reported it here
-  # https://github.com/oboukili/terraform-provider-argocd/issues/196
-  # If the connection is failing, then just remove the comment and try again.
-  # plain_text  = true
-
-  port_forward                = true
-  port_forward_with_namespace = "argocd"
-
-  kubernetes {
-    host                   = var.k8s_host
-    client_certificate     = base64decode(var.k8s_client_certificate)
-    client_key             = base64decode(var.k8s_client_key)
-    cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
-  }
-}
-
-provider "kubectl" {
-  host                   = var.k8s_host
-  cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
-  client_certificate     = base64decode(var.k8s_client_certificate)
-  client_key             = base64decode(var.k8s_client_key)
-}
-
-provider "kubernetes" {
-  host                   = var.k8s_host
-  cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
-  client_certificate     = base64decode(var.k8s_client_certificate)
-  client_key             = base64decode(var.k8s_client_key)
 }
