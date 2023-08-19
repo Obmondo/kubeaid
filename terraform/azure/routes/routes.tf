4b8c7c64d958cd768df65bc6defca6d7702c6d0c
Divyam Azad <divyam@obmondo.com>
2023-08-19 18:09:47 +0200

    [#13613] Add support to add only routes to existing routetable

--- terraform/azure/routes/routes.tf
+++ terraform/azure/routes/routes.tf
@@ -2,20 +2,12 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_route_table" "routetable" {
-  name                          = var.routetable_name
-  location                      = var.location
-  resource_group_name           = "MC_${var.resource_group}_${var.cluster_name}_${var.location}"
-  disable_bgp_route_propagation = var.disable_bgp_route_propagation
-
-  dynamic "route" {
-    for_each = var.routes
-
-    content {
-      name  = route.value.routetable_route_name
-      address_prefix  = route.value.address_prefix
-      next_hop_type = route.value.next_hop_type
-      next_hop_in_ip_address  = route.value.next_hop_in_ip_address
-    }
-  }
+resource "azurerm_route" "routes" {
+  for_each =  var.routes
+  name                = each.key
+  resource_group_name = "MC_${var.resource_group}_${var.cluster_name}_${var.location}"
+  route_table_name    = var.routetable_name
+  address_prefix      = each.value.address_prefix
+  next_hop_type       = each.value.next_hop_type
+  next_hop_in_ip_address = each.value.next_hop_in_ip_address
 }
