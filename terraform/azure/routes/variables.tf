32ee127eda1153edc7987c3e9233770b2fcb547b
Divyam Azad <divyam@obmondo.com>
2023-08-19 18:09:47 +0200

    [#13613] Add support to add only routes to existing routetable

--- terraform/azure/routes/variables.tf
+++ terraform/azure/routes/variables.tf
@@ -16,17 +16,7 @@ variable "cluster_name" {
   type = string
 }
 
-variable "disable_bgp_route_propagation" {
-  type = bool
-  default = false
-}
-
 variable "routes" {
-  description = "List of route configurations"
-  type = list(object({
-    routetable_route_name   = string
-    address_prefix       = string
-    next_hop_type        = string
-    next_hop_in_ip_address = string
-  }))
+  type = map
+  default = {}
 }
